bbdd3c7e29c31efe9ed2009bd98936a871feceb0
dagon1093 <dagon1093@gmail.com>
2025-04-22 00:10:47 +0300

    Добавлена dto и маппер

---
+++ ToDoApi/Dtos/TodoItemDto.cs
@@ -0,0 +1,11 @@
+﻿using ToDoApi.Models;
+
+namespace ToDoApi.Dtos
+{
+    public class TodoItemDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Models.TaskStatus Status { get; set; }
+    }
+}
